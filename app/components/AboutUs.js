33d3458f487e9cd3bd1e53997474407c3eb6ef6a
Dia8897 <khalildia831@gmail.com>
2025-06-19 13:01:18 +0300

    Update AboutUs.js

--- app/components/AboutUs.js
+++ app/components/AboutUs.js
@@ -2,7 +2,7 @@
 
 export default function AboutUs() {
   return (
-    <section className="bg-gray-50 max-w-5xl mx-auto rounded-3xl py-24 px-6">
+    <section className="bg-gray-50 max-w-5xl mx-auto rounded-3xl py-24 px-6 mb-25">
       <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-16 items-center">
         <div className="relative overflow-hidden rounded-2xl shadow-xl max-w-xs max-h-xs mx-auto md:mx-0 group">
           <img
